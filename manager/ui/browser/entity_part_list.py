3cde6c7c8da52874278ebfaebad32ba2e0a254c0
oreyato <oreyato.pro@gmail.com>
2022-11-03 17:02:52 +0100

    ref: better browsing WIP

--- manager/ui/browser/entity_part_list.py
+++ manager/ui/browser/entity_part_list.py
@@ -1,40 +1,71 @@
 import Qt as Qt
+import Qt.QtCore as QtCore
 
-from Qt import QtWidgets
-from Qt.QtWidgets import QWidget, QLabel, QVBoxLayout, QHBoxLayout, QListWidget
+from Qt.QtWidgets import QLayout, QVBoxLayout, QMainWindow, QApplication, QLabel, QWidget, QListWidget
+
+from Qt import QtWidgets, QtCompat
+from manager.ui.window import Window
 
 
-class EntityPartList:
+class EntityPartList(QtWidgets.QWidget):
 
-    def __init__(self, label_p, entity_p):
+    def __init__(self, label_p, entity_p, window_p):
+        super(EntityPartList, self).__init__()
         self.label = label_p
         self.entity = entity_p
+        self.window = window_p
 
     def init_list(self):
+        # Prepare base name
+        base_name = f'{self.label}_ent_part'
+
+        # Prepare layout name
+        layout_name = f'{base_name}_layout'
         # Create layout
         layout = QVBoxLayout()
+        # Modify layout name
+        layout.setObjectName(layout_name)
+
         # v Create label =================================================
         # Prepare label widget name
-        label_name = f'{self.label}'
+        label_name = f'{base_name}_label'
         # Create label widget
-        entity_part_label = QLabel()
+        entity_part_label = QLabel(label_name, self.window)
         # Set its text
         entity_part_label.setText(self.label)
         # Set its alignment
-        entity_part_label.setAlignmnent(Qt.AlignCenter)
+        entity_part_label.setAlignment(QtCore.Qt.AlignCenter)
         # ^ ==============================================================
+
+        # Prepare list widget name
+        list_name = f'{base_name}_list'
         # Create list widget
         entity_part_list = QListWidget()
+        # Resize it
+        entity_part_list.resize(300, 120)
+        # Add an element inside
+        entity_part_list.addItem("Item 1")
 
-        # Add the widgets inside the layout
+        # Add the widgets inside the layouts
         layout.addWidget(entity_part_label)
-        layout.addWidget()
+        layout.addWidget(entity_part_list)
 
-        pass
+        return layout
 
 
 # Create a "main" to test the class
 if __name__ == "__main__":
+    app = QtWidgets.QApplication()
 
-    ep = EntityPartList("label", "entity")
-    print(ep)
+    window = QMainWindow()
+
+    ep = EntityPartList("category", "entity", window)
+    layout = ep.init_list()
+
+    ep.setLayout(layout)
+
+
+    ep.show()
+
+    app.exec_()
+
